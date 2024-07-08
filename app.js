3732683f362b310d00947e6a74992b850a7618eb
ausaafali <ausaf.pbhub005@gmail.com>
2024-07-08 11:35:16 +0500

    re

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.querySelector(".toggle").addEventListener("click", function () {
 window.onscroll = function () {
   myFunction();
 };
+
 let screenHeight =
   (window.scrollY /
     (document.documentElement.scrollHeight - window.innerHeight)) *
@@ -56,31 +57,77 @@ function myFunction() {
       .getElementsByClassName("circler")[4]
       .classList.remove("colored-circle");
   }
-
-//   if (Number(screenHeight) >= 10 && Number(screenHeight) <= 20) {
-//     console.log(document.getElementById("first-head-a"));
-//     document.getElementById("first-head-a").style.display = "none";
-//     document.getElementById("first-head-b").style.display = "block";
-//   } else {
-//     document.getElementById("first-head-a").style.display = "block";
-//     document.getElementById("first-head-b").style.display = "none";
-//   }
 }
-
 
 let blogs_section = document.getElementById("blogs-section");
 let countdownCalled = false;
 if (blogs_section) {
   window.addEventListener("scroll", () => {
-    if (!countdownCalled && eval(window.scrollY + 10) >= blogs_section.offsetTop) {
-      console.log('test');
-      $('.line-toggle').css('background-color','white');
-      $('#in-svg').attr('fill','#fff');
-    }else{
-      $('.line-toggle').css('background-color','black');
-      $('#in-svg').attr('fill','#000');
-      
+    if (
+      !countdownCalled &&
+      eval(window.scrollY + 10) >= blogs_section.offsetTop
+    ) {
+      console.log("test");
+      $(".line-toggle").css("background-color", "white");
+      $("#in-svg").attr("fill", "#fff");
+    } else {
+      $(".line-toggle").css("background-color", "black");
+      $("#in-svg").attr("fill", "#000");
     }
   });
 }
 
+// CARDS SERV
+document.addEventListener("scroll", function () {
+  const cards = [
+    document.getElementById("vid-card-1"),
+    document.getElementById("vid-card-2"),
+    document.getElementById("vid-card-3"),
+  ];
+  const overlapThreshold = 400; //(to change when to blur)
+
+  for (let i = 0; i < cards.length - 1; i++) {
+    const currentCard = cards[i];
+    const nextCard = cards[i + 1];
+    const currentCardRect = currentCard.getBoundingClientRect();
+    const nextCardRect = nextCard.getBoundingClientRect();
+
+    if (nextCardRect.top <= currentCardRect.bottom - overlapThreshold) {
+      currentCard.classList.add("blur");
+    } else {
+      currentCard.classList.remove("blur");
+    }
+  }
+});
+// CARDS SERV
+
+// SERVICES
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    var scrollPosition = $(this).scrollTop();
+    var section2Offset = $("#yinyan-2").offset().top;
+    var section3Offset = $("#yinyan-3").offset().top; // Ensure this matches your third section ID
+
+    console.log(section3Offset); // Check the offset value in the console
+
+    if (scrollPosition >= section3Offset) {
+      $(".section").removeClass("active"); // Remove active class from all sections
+      $("#yinyan-3").addClass("active"); // Add active class to section 3
+    } else if (scrollPosition >= section2Offset) {
+      $(".section").removeClass("active"); // Remove active class from all sections
+      $("#yinyan-2").addClass("active"); // Add active class to section 2
+    } else {
+      $(".section").removeClass("active"); // Remove active class from all sections
+      $("#yinyan-1").addClass("active"); // Add active class to section 1 (default)
+    }
+  });
+});
+// SERVICES
+
+// random img
+
+// random img
+
+// svgs
+
+// svgs
